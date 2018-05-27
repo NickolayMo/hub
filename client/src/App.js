919460c0dca75f82c209f561e0dc41a91e309c3c
Mokrenko Nickolay <kolya456@gmail.com>
2018-05-27 21:12:38 +0300

    Форматирование

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,28 @@ import Post from './components/Post/Post';
 import {pullPosts} from './actions';
 
 class App extends Component {
-    
-    componentDidMount(){
-        pullPosts().catch(function(error) {console.log(error)});
+
+    componentDidMount() {
+        pullPosts().catch(function (error) {
+            console.log(error)
+        });
     }
-  render() {
-    return (
-        <BrowserRouter>
-          <div className="app container">
-              <nav className="app__header navbar navbar-light bg-light">
-                  <Link className="navbar-brand" to={'/'}>Hub</Link>
-              </nav>
-              <Route exact path='/:p?' component={PostList}/>
-              <Route path='/post/:id' component={Post}/>
-              <div className="app__footer bg-light">
-              </div>
-          </div>
-        </BrowserRouter>
-    );
-  }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="app container">
+                    <nav className="app__header navbar navbar-light bg-light">
+                        <Link className="navbar-brand" to={'/'}>Hub</Link>
+                    </nav>
+                    <Route exact path='/:p?' component={PostList}/>
+                    <Route path='/post/:id' component={Post}/>
+                    <div className="app__footer bg-light">
+                    </div>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
