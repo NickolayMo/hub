ab4fb2e9c18f36c388df7ef92d677abae2f7e04a
NORBIT\Nikolay.Mokrenko <Nikolay.Mokrenko@norbit.ru>
2018-07-24 14:10:13 +0300

    Правки стилей

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import PostSearch from "../../containers/PostSearch/PostSearch";
+import './Header.css';
 
 class Header extends React.Component
 {
     render(){
         return (
-            <nav className="app__header navbar navbar-light bg-light">
+            <nav className="header navbar navbar-light bg-light">
                 <Link className="navbar-brand" to={'/'}>Hub</Link>
                 <PostSearch/>
             </nav>
